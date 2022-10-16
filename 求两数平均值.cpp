95f8ad81ace054fe8fa0cba48b15fb6f3ee3e49f
cipia <114488416+cipia@users.noreply.github.com>
2022-10-16 20:56:41 +0800

    Add files via upload

---
+++ 求两数平均值.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+int main()
+{
+	int a,b,sum;
+	a=0;
+	printf("A=");
+	scanf("%d",&a);
+	b=0;
+	printf("B=");
+	scanf("%d",&b);
+
+
+	sum=(a+b)/2;
+	printf("sum=%d\n",sum);
+	return 0;
+	
+ } 
